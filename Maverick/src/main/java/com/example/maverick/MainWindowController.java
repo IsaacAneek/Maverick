a955f1d42b37a70ae31e928d432a66e87c0356ab
Rana094 <81284474+IsaacAneek@users.noreply.github.com>
2025-12-19 22:37:12 +0600

    Updated CSS elements for textField and replaced labels with textFields

--- Maverick/src/main/java/com/example/maverick/MainWindowController.java
+++ Maverick/src/main/java/com/example/maverick/MainWindowController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -15,20 +16,32 @@ import java.io.IOException;
 public class MainWindowController {
     @FXML private VBox todoVbox;
     @FXML private VBox ongoingVbox;
+    @FXML private VBox doneVbox;
     @FXML private HBox dashboardHbox;
     @FXML private VBox miscVbox;
 
     @FXML
     protected void onTodoAddCardClicked(ActionEvent event) {
-        Label label = new Label("New to do");
-        label.setPrefWidth(todoVbox.getWidth() - 10);
-        label.setMaxWidth(Double.MAX_VALUE);
-        todoVbox.getChildren().add(label);
+        TextField text = new TextField();
+        text.setPrefWidth(todoVbox.getWidth() - 10);
+        text.setMaxWidth(Double.MAX_VALUE);
+        todoVbox.getChildren().add(text);
     }
 
     @FXML
     protected void onOngoingAddCardClicked(ActionEvent event) {
+        TextField text = new TextField();
+        text.setPrefWidth(todoVbox.getWidth() - 10);
+        text.setMaxWidth(Double.MAX_VALUE);
+        ongoingVbox.getChildren().add(text);
+    }
 
+    @FXML
+    protected void onDoneAddCardClicked(ActionEvent event) {
+        TextField text = new TextField();
+        text.setPrefWidth(todoVbox.getWidth() - 10);
+        text.setMaxWidth(Double.MAX_VALUE);
+        doneVbox.getChildren().add(text);
     }
 
     @FXML
