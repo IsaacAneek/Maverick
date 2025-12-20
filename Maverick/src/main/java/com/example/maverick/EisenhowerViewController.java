c7709214bd2686004b9cf28a626fa650f8c2eb11
Rana094 <81284474+IsaacAneek@users.noreply.github.com>
2025-12-20 16:51:13 +0600

    Add task feature in eisenhower matrix

--- Maverick/src/main/java/com/example/maverick/EisenhowerViewController.java
+++ Maverick/src/main/java/com/example/maverick/EisenhowerViewController.java
@@ -2,18 +2,42 @@ package com.example.maverick;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class EisenhowerViewController {
 
     public void onAddImportantUrgentClicked(ActionEvent actionEvent) {
+        TextField txt = new TextField();
+        txt.setAlignment(Pos.CENTER);
+        VBox parentVbox = (VBox) ((Button) actionEvent.getSource()).getParent();
+        VBox taskList = (VBox) parentVbox.getChildren().get(1);
+        taskList.getChildren().add(txt);
     }
 
     public void onNotImportantUrgentClicked(ActionEvent actionEvent) {
+        TextField txt = new TextField();
+        txt.setAlignment(Pos.CENTER);
+        VBox parentVbox = (VBox) ((Button) actionEvent.getSource()).getParent();
+        VBox taskList = (VBox) parentVbox.getChildren().get(0);
+        taskList.getChildren().add(txt);
     }
 
     public void onAddImportantNotUrgentClicked(ActionEvent actionEvent) {
+        TextField txt = new TextField();
+        txt.setAlignment(Pos.CENTER);
+        VBox parentVbox = (VBox) ((Button) actionEvent.getSource()).getParent();
+        VBox taskList = (VBox) parentVbox.getChildren().get(1);
+        taskList.getChildren().add(txt);
     }
 
     public void onNotImportantNotUrgentClicked(ActionEvent actionEvent) {
+        TextField txt = new TextField();
+        txt.setAlignment(Pos.CENTER);
+        VBox parentVbox = (VBox) ((Button) actionEvent.getSource()).getParent();
+        VBox taskList = (VBox) parentVbox.getChildren().get(0);
+        taskList.getChildren().add(txt);
     }
 }
