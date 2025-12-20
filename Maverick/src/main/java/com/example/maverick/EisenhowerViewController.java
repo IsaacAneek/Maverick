5e1469700e1353cc79d315bf94184abdeea38115
Rana094 <81284474+IsaacAneek@users.noreply.github.com>
2025-12-20 16:12:07 +0600

    Created eisenhower view and its controller

---
+++ Maverick/src/main/java/com/example/maverick/EisenhowerViewController.java
@@ -0,0 +1,19 @@
+package com.example.maverick;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EisenhowerViewController {
+
+    public void onAddImportantUrgentClicked(ActionEvent actionEvent) {
+    }
+
+    public void onNotImportantUrgentClicked(ActionEvent actionEvent) {
+    }
+
+    public void onAddImportantNotUrgentClicked(ActionEvent actionEvent) {
+    }
+
+    public void onNotImportantNotUrgentClicked(ActionEvent actionEvent) {
+    }
+}
